5f57b2567be57cf505bf33e310be5974ed42aff8
IgorKrot <108532514+IgorKrot@users.noreply.github.com>
2022-09-14 11:48:44 +0300

    JS to TypeScript
    JS to TypeScript

--- src/utils/calcTotalCostFromLS.jsx
+++
@@ -1,4 +0,0 @@
-export const calcTotalCostFromLS = (items) => {
-   return items.reduce((sumPrice, currentPrice) => {return sumPrice + (currentPrice.price * currentPrice.count)},
-      0);
-};
--- src/utils/getDataFromLS.jsx
+++
@@ -1,8 +0,0 @@
-import { calcTotalCostFromLS } from "../../src/utils/calcTotalCostFromLS"
-
-export const getDataFromLS = () => {
-   const data = localStorage.getItem("cart");
-   const items = data ? JSON.parse(data) : [];
-   const totalCost = calcTotalCostFromLS(items);
-   return {items, totalCost,};
-};
